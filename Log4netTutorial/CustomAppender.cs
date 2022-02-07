27bfb41f7e632b8095eb6b54172fea897f02293f
Paul Amazona <whatevergeek@gmail.com>
2022-02-07 10:16:48 +0800

    Add project files.

---
+++ Log4netTutorial/CustomAppender.cs
@@ -0,0 +1,16 @@
+﻿using log4net.Appender;
+using log4net.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Log4netTutorial
+{
+    public class CustomAppender : AppenderSkeleton
+    {
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            Console.WriteLine($"{loggingEvent.Level.Name}: {RenderLoggingEvent(loggingEvent)}");
+        }
+    }
+}
---
+++ Log4netTutorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using log4net.Repository;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Log4netTutorial
+{
+    class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            try
+            {
+                ILoggerRepository repository = log4net.LogManager.GetRepository(Assembly.GetCallingAssembly());
+
+                var fileInfo = new FileInfo(@"log4net.config");
+
+                log4net.Config.XmlConfigurator.Configure(repository, fileInfo);
+
+                log.Info("test");
+
+                Console.WriteLine("press any key");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Press any key to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
